50346537c2a7d59e7302a42a4130bb41ef8a5866
bcwagne <98066633+bcwagne@users.noreply.github.com>
2023-07-04 18:54:06 -0600

    add itoa function

--- ulib.c
+++ ulib.c
@@ -104,3 +104,38 @@ memmove(void *vdst, const void *vsrc, int n)
     *dst++ = *src++;
   return vdst;
 }
+
+char * itoa(int num) { 
+// This function converts a positive or negative decimal integer to ASCII
+    static char str[20]; // return string, static = not on the stack
+    int i = 0;           // index for string when adding digits
+    int start, end;      // used to reverse the digits;
+    int isneg = (num < 0); // integer (boolean) indicating num is negative
+    char tmpchar;        // temp variable used to swap characters
+
+    if (num == 0) { // zero is a special case
+        str[i++] = '0'; // set string to "0"
+        str[i] = '\0'; // NULL terminate string
+        return str; // return with the string, "0"
+    }     // None of the rest executes for zero
+
+    while (num != 0) { // not zero, convert each digit
+        str[i++] = (num % 10) + '0'; // add ASCII char in reverse order
+        num = num / 10; // go to next digit
+    }
+
+    if (isneg) str[i++] = '-'; // if negative add the minus sign
+    str[i] = '\0'; // NULL terminate the string
+
+    // now reverse the string
+    start = 0; // initialize start and end to the first and last ASCII 
+    end = i - 1; // character positions (not the NULL)
+    while (start < end) { // swap the char at end with the one at start
+        tmpchar = str[start];
+        str[start] = str[end];
+        str[end] = tmpchar;
+        start++; 
+        end--;
+    }
+    return str; 
+}
